a05c3082687aad28a99b033700ed85f8b30bcc19
Alexander Mora Ruiz <alex16mr@gmail.com>
2023-03-21 00:13:48 -0500

    update spec file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clojure-challenge "0.1.0-SNAPSHOT"
   :description "clojure-challenge"
-  :dependencies [[org.clojure/data.json "0.2.6"]
+  :dependencies [[org.clojure/data.json "2.4.0"]
                  [cheshire "5.11.0"]])
--- src/invoice_spec.clj
+++ src/invoice_spec.clj
@@ -1,24 +1,62 @@
 (ns invoice-spec
-  (:require
-   [clojure.spec.alpha :as s]
-   [cheshire.core :as json]))
+  (:require [clojure.data.json :as json]
+            [clojure.spec.alpha :as s]))
 
-(s/def ::tax-category keyword?)
-(s/def ::tax-rate float?)
-(s/def ::taxes (s/keys :req-un [::tax-category ::tax-rate]))
-(s/def ::customer (s/keys :req-un [::company-name ::email]))
-(s/def ::items (s/keys :req-un [::price ::quantity ::sku ::taxes]))
-(s/def ::retentions (s/keys :req-un [::tax-category ::tax-rate]))
-(s/def ::invoice (s/keys :req-un [::issue-date ::order-reference ::payment-date
-                                  ::payment-means ::payment-means-type ::number
-                                  ::customer ::items ::retentions]))
+(s/def :customer/company-name string?)
+(s/def :customer/email string?)
+(s/def :invoice/customer (s/keys :req [:customer/company-name
+                                       :customer/email]))
 
-(defn convert-invoice [filename]
+(s/def :tax/tax-rate double?)
+(s/def :tax/tax-category #{:iva})
+(s/def ::tax (s/keys :req [:tax/tax-category
+                           :tax/tax-rate]))
+
+(s/def :invoice-item/taxes (s/coll-of ::tax :kind vector? :min-count 1))
+
+(s/def :invoice-item/price double?)
+(s/def :invoice-item/quantity double?)
+(s/def :invoice-item/sku string?)
+
+(s/def ::invoice-item
+  (s/keys :req [:invoice-item/price
+                :invoice-item/quantity
+                :invoice-item/sku
+                :invoice-item/taxes]))
+
+(s/def ::retention (s/keys :req [:tax/tax-category
+                                 :tax/tax-rate]))
+
+(s/def :invoice/issue-date inst?)
+(s/def :invoice/order-reference string?)
+(s/def :invoice/payment-date string?)
+(s/def :invoice/payment-means string?)
+(s/def :invoice/payment-means-type string?)
+(s/def :invoice/number double?)
+
+(s/def :invoice/items (s/coll-of ::invoice-item :kind vector? :min-count 1))
+(s/def :invoice/retentions (s/coll-of ::retention :kind vector? :min-count 1))
+
+(s/def ::invoice
+  (s/keys :req [:invoice/issue-date
+                :invoice/customer
+                :invoice/items
+                :invoice/retentions
+                :invoice/order-reference
+                :invoice/payment-date
+                :invoice/payment-means
+                :invoice/payment-means-type
+                :invoice/number]))
+
+
+(defn read-invoice [filename]
   (let [json-str (slurp filename)
-        invoice-map (json/parse-string json-str true)
-        conform-result (s/conform ::invoice invoice-map)]
-    (if (s/valid? ::invoice conform-result)
-      conform-result
+        invoice (json/read-str json-str)]
+    (print json-str)
+    (print invoice)
+    (if (s/valid? ::invoice invoice)
+      invoice
       nil)))
 
-(convert-invoice "E:\\Data\\alex\\Dev\\clojure-challenge\\invoice.json")
+(let [invoice (read-invoice "E:\\Data\\alex\\Dev\\clojure-challenge\\invoice.json")]
+  (println invoice))
