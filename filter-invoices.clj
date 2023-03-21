93df87dbe0cab2a03129363ce964edefda250a0a
Alexander Mora Ruiz <alex16mr@gmail.com>
2023-03-20 21:06:39 -0500

    Refactor function to filter invoices

--- filter-invoices.clj
+++ filter-invoices.clj
@@ -1,12 +1,25 @@
 (def invoice (clojure.edn/read-string (slurp "invoice.edn")))
 
-(defn get-invoice-items [invoice]
-  (->> (:invoice/items invoice)
-       (filter (fn [item]
-                 (let [has-iva-19? (-> item :taxable/taxes
-                                       (some #(and (= (:tax/category %) :iva)
-                                                   (= (:tax/rate %) 19))))
-                       has-ret-1? (-> item :retentionable/retations
-                                      (some #(and (= (:retention/category %) :ret_fuente)
-                                                  (= (:retention/rate %) 1))))]
-                   )))))
+(defn filter-invoice-items [invoice]
+      (->> invoice
+           :invoice/items
+           (filter (fn [param1]
+                       (or (some #(= 19 (:tax/rate %)) (:taxable/taxes param1))
+                           (some #(= 1 (:retention/rate %)) (:retentionable/retentions param1)))))
+           (filter (fn [param1]
+                       (not (some #(= 19 (:tax/rate %)) (:taxable/taxes param1))
+                            (some #(= 1 (:retention/rate %)) (:retentionable/retentions param1)))))))
+
+
+(filter-invoice-items invoice)
+
+;; {:invoice-item/id  "ii3"
+;;                  :invoice-item/sku "SKU 3"
+;;                 :taxable/taxes    [{:tax/id       "t3"
+;;                                      :tax/category :iva
+;;                                      :tax/rate     19}]}
+;;                 {:invoice-item/id          "ii4"
+;;                  :invoice-item/sku         "SKU 3"
+;;                  :retentionable/retentions [{:retention/id       "r2"
+;;                                              :retention/category :ret_fuente
+;;                                              :retention/rate     1}]}
