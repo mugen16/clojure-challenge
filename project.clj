71c525b2b9a99b7e578f9d65ac7e55e1fbf98fc5
Alexander Mora Ruiz <alex16mr@gmail.com>
2023-03-21 21:24:36 -0500

    Fix: challenge two

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-challenge "0.1.0-SNAPSHOT"
   :description "clojure-challenge"
   :dependencies [[org.clojure/data.json "2.4.0"]
-                 [cheshire "5.11.0"]])
+                 [org.clojure/test.check "1.1.1"]
+                 [clj-time "0.15.2"]])
--- src/invoice_spec.clj
+++ src/invoice_spec.clj
@@ -1,17 +1,17 @@
 (ns invoice-spec
   (:require [clojure.data.json :as json]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [clojure.string :as string]))
 
-(s/def :customer/company-name string?)
+(s/def :customer/name string?)
 (s/def :customer/email string?)
-(s/def :invoice/customer (s/keys :req [:customer/company-name
+(s/def :invoice/customer (s/keys :req [:customer/name
                                        :customer/email]))
 
-(s/def :tax/tax-rate double?)
-(s/def :tax/tax-category #{:iva})
-(s/def ::tax (s/keys :req [:tax/tax-category
-                           :tax/tax-rate]))
-
+(s/def :tax/rate double?)
+(s/def :tax/category #{:iva})
+(s/def ::tax (s/keys :req [:tax/category
+                           :tax/rate]))
 (s/def :invoice-item/taxes (s/coll-of ::tax :kind vector? :min-count 1))
 
 (s/def :invoice-item/price double?)
@@ -24,39 +24,66 @@
                 :invoice-item/sku
                 :invoice-item/taxes]))
 
-(s/def ::retention (s/keys :req [:tax/tax-category
-                                 :tax/tax-rate]))
-
 (s/def :invoice/issue-date inst?)
-(s/def :invoice/order-reference string?)
-(s/def :invoice/payment-date string?)
-(s/def :invoice/payment-means string?)
-(s/def :invoice/payment-means-type string?)
-(s/def :invoice/number double?)
-
 (s/def :invoice/items (s/coll-of ::invoice-item :kind vector? :min-count 1))
-(s/def :invoice/retentions (s/coll-of ::retention :kind vector? :min-count 1))
 
 (s/def ::invoice
   (s/keys :req [:invoice/issue-date
                 :invoice/customer
-                :invoice/items
-                :invoice/retentions
-                :invoice/order-reference
-                :invoice/payment-date
-                :invoice/payment-means
-                :invoice/payment-means-type
-                :invoice/number]))
+                :invoice/items]))
+
+(def key-map-namespace {:issue_date "invoice"
+                        :customer "invoice"
+                        :items "invoice"
+                        :order_reference "invoice"
+                        :payment_date "invoice"
+                        :payment_means "invoice"
+                        :payment_means_type "invoice"
+                        :number "invoice"
+                        :email "customer"
+                        :rate "tax"
+                        :category "tax"
+                        :taxes "invoice-item"
+                        :price "invoice-item"
+                        :quantity "invoice-item"
+                        :sku "invoice-item"
+                        :tax_category "tax"
+                        :tax_rate "tax"
+                        :retentions "invoice"
+                        :name "customer"})
 
 
-(defn read-invoice [filename]
-  (let [json-str (slurp filename)
-        invoice (json/read-str json-str)]
-    (print json-str)
-    (print invoice)
-    (if (s/valid? ::invoice invoice)
-      invoice
-      nil)))
+(defn parse-date [str-date]
+  (.parse (java.text.SimpleDateFormat. "dd/MM/yyyy") str-date))
 
-(let [invoice (read-invoice "E:\\Data\\alex\\Dev\\clojure-challenge\\invoice.json")]
-  (println invoice))
+(defn convert-value [key value]
+  (if (int? value)
+    (double value)
+    (if (= key :invoice/issue-date)
+      (parse-date value)
+      (if (= key :tax/category)
+        :iva
+        value))))
+
+(defn custom-keyword [key]
+  (if (= key "invoice")
+    (keyword key)
+    (if (= key "tax_category")
+      (keyword (get key-map-namespace (keyword "category")) "category")
+      (if (= key "tax_rate")
+        (keyword (get key-map-namespace (keyword "rate")) "rate")
+        (if (= key "company_name")
+          (keyword (get key-map-namespace (keyword "name")) "name")
+          (keyword (get key-map-namespace (keyword key) "") (string/replace (name key) "_" "-")))))))
+
+(defn read-invoice [file-name]
+  (let [json-str (slurp file-name)
+        invoice-map (json/read-str json-str :key-fn custom-keyword :value-fn convert-value)
+        data (get invoice-map :invoice)]
+    (s/explain ::invoice data)
+    (if (s/valid? ::invoice data)
+      true
+      false)))
+
+(let [result (read-invoice "E:\\Data\\alex\\Dev\\clojure-challenge\\invoice.json")]
+  (println result))
